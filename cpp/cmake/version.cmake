f3d778cd89fe8ddc18d6407de24cea39697035fc
John Patton <jpatton@usgs.gov>
2019-02-13 13:42:08 -0700

    Updated version/made java version checkable

--- cpp/cmake/version.cmake
+++ cpp/cmake/version.cmake
@@ -1,6 +1,6 @@
 # version.cmake - a CMake script that defines the overall project version
-# NOTE: Make sure to also update the java version strings in ClientBase.java
+# NOTE: Make sure to also update the java version strings in Version.java
 # when updating this file
 set (PROJECT_VERSION_MAJOR 0)
 set (PROJECT_VERSION_MINOR 9)
-set (PROJECT_VERSION_PATCH 6)
+set (PROJECT_VERSION_PATCH 8)
